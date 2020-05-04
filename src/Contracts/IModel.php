c13fdf609a060c0615cf281933212dd6efbb8db5
ing.dacastillop <ing.dacastillop@gmail.com>
2020-05-04 08:50:00 -0500

    Agregando funcionalidad para recargar agregaciones del modelo

--- src/Contracts/IModel.php
+++ src/Contracts/IModel.php
@@ -5,5 +5,15 @@ namespace Xofttion\ORM\Contracts;
 use Illuminate\Database\Eloquent\Model;
 
 abstract class IModel extends Model implements IModelORM {
+        
+    // Métodos de la clase IModel
     
+    /**
+     * 
+     * @param array $relationships
+     * @return void
+     */
+    public function reaload(array $relationships): void {
+        $this->load($this->getMapper()->getRelationshipsFormat($relationships));
+    }
 }
